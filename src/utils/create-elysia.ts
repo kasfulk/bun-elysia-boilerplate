a0e93cfa8dcda20272eddb70d7860335ed687a3e
Kasjful Kurniawan <kasful.bpp@gmail.com>
2024-06-19 11:26:58 +0800

    refactor: remove unused jwt access in createElysia

--- src/utils/create-elysia.ts
+++ src/utils/create-elysia.ts
@@ -1,8 +1,4 @@
-import { jwtAccessSetup, jwtRefreshSetup } from '@guards/setup.jwt';
 import { Elysia } from 'elysia';
 export const createElysia = (
   config?: ConstructorParameters<typeof Elysia>[0],
-) =>
-  new Elysia({ ...config, aot: process.env.RUNTIME === 'bun' })
-    .use(jwtAccessSetup)
-    .use(jwtRefreshSetup);
+) => new Elysia({ ...config, aot: process.env.RUNTIME === 'bun' });
